a09b5b7f23bcf539734a0110039d93641c025ff6
Dimitar Bounov <dimitar.bounov@consensys.net>
2024-03-23 03:51:06 +0200

    bump timeout

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -93,7 +93,7 @@ describe(`Instances produce the same output`, () => {
 
                         done();
                     });
-                }, 20000);
+                }, 60000);
             }
         });
     }
